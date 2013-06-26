29ef38325723ba2a2ab61f7caba01f0fb34ab71a
Lu Liang <lulyon@126.com>
2013-06-26 13:17:35 +0800

    Update sctest.cc

--- sctest.cc
+++ sctest.cc
@@ -1,19 +1,23 @@
-/*
- * sctest.cc
- *
- *  Created on: 2013-6-18
- *      Author: luliang
- */
+/// @file sctest.cc
+/// @author luliang@ict.ac.cn
+/// @copybrief Copyright 2012 ICT, CAS. All rights reserved.
+/// @version 0.6
+/// @date 2013-06-18
+
+#include <stdio.h>
+
+#include "spatialClient.h"
+
 
 
 int main()
 {
-  	SpatialClient client;
+	SpatialClient client;
 	if(client.connect()) {
 
 	}
 	else {
-		fprintf(stderr, "Can't connect the redis server.\n");
+		fprintf(stderr, "Can not connect the redis server.\n");
 	}
 	return 0;
 }
