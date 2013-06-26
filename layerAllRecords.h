4d8a0f067f15ac52de547503d0345c79fe1d52ca
Lu Liang <lulyon@126.com>
2013-06-26 13:21:25 +0800

    Update layerAllRecords.h

--- layerAllRecords.h
+++ layerAllRecords.h
@@ -1,7 +1,7 @@
 /// @file layerAllRecords.h
 /// @author luliang@ict.ac.cn
 /// @copybrief Copyright 2012 ICT, CAS. All rights reserved.
-/// @version 0.6
+/// @version 0.1
 /// @date 2013-06-25
 
 #ifndef LAYERALLRECORDS_H_
